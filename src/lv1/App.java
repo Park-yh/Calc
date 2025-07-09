895ac5d1ca8a0989ae0a07db3992d0cad40f0b75
Park-yh <pyh5421@naver.com>
2025-07-09 16:56:08 +0900

    이름 변경

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -2,7 +2,7 @@ package lv1;
 
 import java.util.Scanner;
 
-public class calc {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int result = 0;
